76c9609147a7167033ef0f3af92f40ca64af9116
Yulia Bocieva <yuliabocieva@MacBook-Pro-Aleksandr.local>
2020-01-13 19:05:58 +0300

    homework

---
+++ script2.js
@@ -0,0 +1,75 @@
+/**
+ * Returns the rectagle object with width and height parameters and getArea() method
+ *
+ * @param {number} width
+ * @param {number} height
+ * @return {Object}
+ *
+ * @example
+ *    var r = new Rectangle(10,20);
+ *    console.log(r.width);       // => 10
+ *    console.log(r.height);      // => 20
+ *    console.log(r.getArea());   // => 200
+ */
+function Rectangle(width, height) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Returns the n last items of the specified arrays
+ *
+ * @param {array} arr
+ * @param {number} n
+ *
+ * @example
+ *    [ 1, 3, 4, 5 ], 2  => [ 4, 5 ]
+ *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
+ */
+function getTail(arr, n) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Inserts the item into specified array at specified index
+ *
+ * @param {array} arr
+ * @param {any} item
+ * @param {number} index
+ *
+ * @example
+ *    [ 1, 3, 4, 5 ], 2, 1  => [ 1, 2, 3, 4, 5 ]
+ *    [ 1, 'b', 'c'], 0, 'x'  => [ 'x', 1, 'b', 'c' ]
+ */
+function insertItem(arr, item, index) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Returns an array of positive numbers from the specified array in original order
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *    [ 0, 1, 2, 3, 4, 5 ] => [ 1, 2, 3, 4, 5 ]
+ *    [-1, 2, -5, -4, 0] => [ 2 ]
+ *    [] => []
+ */
+function getArrayOfPositives(arr) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Returns array containing only unique values from the specified array.
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *   [ 1, 2, 3, 3, 2, 1 ] => [ 1, 2, 3 ]
+ *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
+ *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
+ */
+function distinct(arr) {
+  throw new Error('Not implemented');
+}
